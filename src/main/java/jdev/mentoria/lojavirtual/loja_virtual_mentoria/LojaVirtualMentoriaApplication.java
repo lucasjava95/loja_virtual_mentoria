82a171ff99ac15b2ed814890ff7a1dc5f0d1d69e
User <User@LAPTOP-OJC0P9DU>
2024-12-29 17:08:07 -0300

    classe marca_produto criada.

--- src/main/java/jdev/mentoria/lojavirtual/loja_virtual_mentoria/LojaVirtualMentoriaApplication.java
+++ src/main/java/jdev/mentoria/lojavirtual/loja_virtual_mentoria/LojaVirtualMentoriaApplication.java
@@ -2,8 +2,10 @@ package jdev.mentoria.lojavirtual.loja_virtual_mentoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "jdev.mentoria.lojavirtual.model")
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
