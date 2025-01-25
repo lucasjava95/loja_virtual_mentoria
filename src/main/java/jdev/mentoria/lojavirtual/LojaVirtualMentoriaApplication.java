20acf0e789e850316a644cb8d45619065d881193
User <User@LAPTOP-OJC0P9DU>
2025-01-25 18:13:58 -0300

    criacao dos pacotes repository, service e controller

--- src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -1,4 +1,4 @@
-package jdev.mentoria.lojavirtual.loja_virtual_mentoria;
+package jdev.mentoria.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
