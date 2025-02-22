8fc9eeb58332c5c20f96ff2fa1cc46a4e0504d42
User <User@LAPTOP-OJC0P9DU>
2025-02-22 12:11:46 -0300

    Alteracao 22-02-25

--- src/main/java/jdev/mentoria/lojavirtual/model/Usuario.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/Usuario.java
@@ -36,104 +36,117 @@ public class Usuario implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
 	private Long id;
-	
-	
+
 	@Column(nullable = false)
 	private String login;
-	
-	
+
 	@Column(nullable = false)
 	private String senha;
-	
-	
-    @Temporal(TemporalType.DATE)
-    @Column(nullable = false)
+
+	@Temporal(TemporalType.DATE)
+	@Column(nullable = false)
 	Date dataAtualSenha;
-    
-    
-    @ManyToOne(targetEntity = Pessoa.class)
+
+	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
 	private Pessoa pessoa;
-    
-    
-    
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "usuarios_acesso", uniqueConstraints = @UniqueConstraint(columnNames = 
-    {"usuario_id", "acesso_id"}, name = "unique_acesso_user"), 
-     joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario",
-     unique = false, foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)),
-     
-     
-     
-     inverseJoinColumns = @JoinColumn(name = "acesso_id", referencedColumnName = "id", table = "acesso",
-     unique = false, foreignKey = @ForeignKey(name = "acesso_fk", value = ConstraintMode.CONSTRAINT)))
-    private List<Acesso> acessos;
-    
-    
-    
-    public void setPessoa(Pessoa pessoa) {
+
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "usuarios_acesso", uniqueConstraints = @UniqueConstraint(columnNames = { "usuario_id",
+			"acesso_id" }, name = "unique_acesso_user"), joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario", unique = false, foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)),
+
+	inverseJoinColumns = @JoinColumn(name = "acesso_id", referencedColumnName = "id", table = "acesso", unique = false, foreignKey = @ForeignKey(name = "acesso_fk", value = ConstraintMode.CONSTRAINT)))
+	private List<Acesso> acessos;
+
+	
+	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
 	}
-    
-    
-    
-    public Pessoa getPessoa() {
+
+	public Pessoa getPessoa() {
 		return pessoa;
 	}
-	
-	
-	
-    /*Autoridades são os acessos, ex: ROLE_ADMIN, ROLE_SECRETARIO*/
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Date getDataAtualSenha() {
+		return dataAtualSenha;
+	}
+
+	public void setDataAtualSenha(Date dataAtualSenha) {
+		this.dataAtualSenha = dataAtualSenha;
+	}
+
+	public List<Acesso> getAcessos() {
+		return acessos;
+	}
+
+	public void setAcessos(List<Acesso> acessos) {
+		this.acessos = acessos;
+	}
+
+	/* Autoridades são os acessos, ex: ROLE_ADMIN, ROLE_SECRETARIO */
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		
-		
+
 		return this.acessos;
-	
-	
-	
+
 	}
 
 	@Override
 	public String getPassword() {
-		
+
 		return this.senha;
-	
-	
-	
+
 	}
 
 	@Override
 	public String getUsername() {
-		
+
 		return this.login;
-	
-	
+
 	}
 
-	
-	
-	
 	/*
-	@Override
-	public boolean isAccountNonExpired() {
-
-		return true;
-
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-
-		return true;
-
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-
-		return true;
-
-	}*/
+	 * @Override public boolean isAccountNonExpired() {
+	 * 
+	 * return true;
+	 * 
+	 * }
+	 * 
+	 * @Override public boolean isAccountNonLocked() {
+	 * 
+	 * return true;
+	 * 
+	 * }
+	 * 
+	 * @Override public boolean isCredentialsNonExpired() {
+	 * 
+	 * return true;
+	 * 
+	 * }
+	 */
 
 }
--- src/main/java/jdev/mentoria/lojavirtual/security/WebConfigSecurity.java
+++ src/main/java/jdev/mentoria/lojavirtual/security/WebConfigSecurity.java
@@ -1,34 +1,82 @@
 package jdev.mentoria.lojavirtual.security;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import jakarta.activation.DataSource;
+import jdev.mentoria.lojavirtual.service.ImplementacaoUserDetailsService;
 
 
 @Configuration
 @EnableWebSecurity
 public class WebConfigSecurity {
 	
+	
+	@Autowired
+	private ImplementacaoUserDetailsService userDetailsService;
+	
  
 	@Bean
     public WebSecurityCustomizer webSecurityCustomizer() throws Exception{
     	
     	
-    	
+    	/*URLs para se ignorar (não autenticar)*/
     	 return (web) -> web.ignoring().requestMatchers(HttpMethod.POST, "/salvarAcesso")
     			 
     			 .requestMatchers(HttpMethod.DELETE, "/deleteAcesso");
-		
-		
-    	
-    	
-    	
+	
     }
+	
+	
+	/*
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+	
+		http
+        .authorizeHttpRequests((authz) -> authz
+            .anyRequest().authenticated()
+        
+        		
+        );
+      return http.build();  
+		
+		
+		
+		
+	}*/
+	
+	
+    @Bean
+	public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+		
+		
+	DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+	
+	
+	provider.setUserDetailsService(userDetailsService);
+	
+	provider.setPasswordEncoder(passwordEncoder);
+	
+	
+	return new ProviderManager(provider);
+		
+		
+		
+	}
 
-	
-	
 	
 
 }
