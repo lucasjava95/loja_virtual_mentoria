c65d28d17f6ac6b9f52f1c95f1bb4688bfb8b3af
User <User@LAPTOP-OJC0P9DU>
2025-02-27 13:15:59 -0300

    alteracao JWTTokenAutenticacaoService

--- src/main/java/jdev/mentoria/lojavirtual/security/JWTTokenAutenticacaoService.java
+++ src/main/java/jdev/mentoria/lojavirtual/security/JWTTokenAutenticacaoService.java
@@ -2,12 +2,18 @@ package jdev.mentoria.lojavirtual.security;
 
 import java.util.Date;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jdev.mentoria.lojavirtual.ApplicationContextLoad;
+import jdev.mentoria.lojavirtual.model.Usuario;
+import jdev.mentoria.lojavirtual.repository.UsuarioRepository;
 
 
 /*Classe que cria e retorna a autenticacão JWT*/
@@ -54,6 +60,8 @@ public class JWTTokenAutenticacaoService {
 	  response.addHeader(HEADER_STRING, token);
 	  
 	  
+	  liberacaoCors(response);
+	  
 	  
 	  /*seta para o corpo da resposta (mais usado para o Postman)*/
 	  response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
@@ -62,6 +70,102 @@ public class JWTTokenAutenticacaoService {
 	}
 	
 	
+	/*Faz a validacao do usuario/token a cada requisição que o usuario faz (salvar, consultar, deletar, etc) e, caso nao validado, retorna Null*/
+	
+	public Authentication getAuthentication(HttpServletRequest request, HttpServletResponse response) {
+		
+		String token = request.getHeader(HEADER_STRING);
+		
+		if(token != null) {
+			
+			
+		  String tokenSemBearer = token.replace(TOKEN_PREFIX, "").trim();
+			
+			
+		  /*obtem o USER*/
+		  String user = Jwts.parser().
+				  
+				        setSigningKey(SECRET)
+				        
+				        .parseClaimsJws(tokenSemBearer)
+				        
+				        .getBody().getSubject();
+		  
+		  
+		  if(user != null) {
+			  
+			  
+		  Usuario usuario = ApplicationContextLoad.
+				  
+				             getApplicationContext().
+				  
+				             getBean(UsuarioRepository.class).findUserByLogin(user);
+		  
+		  
+		  if(usuario != null) {
+			  
+			  
+			  return new UsernamePasswordAuthenticationToken(usuario.getLogin(), usuario.getSenha(), usuario.getAuthorities());
+			  
+			  
+		  }
+		  
+			  
+			  
+		  
+		  }
+		  	
+			
+			
+		
+		}
+		
+		liberacaoCors(response);
+		return null;
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	/*liberacao contra erro de Cors no navegador*/
+	public void liberacaoCors(HttpServletResponse response) {
+		
+		if(response.getHeader("Access-Control-Allow-Origin") == null) {
+			
+			response.addHeader("Access-Control-Allow-Origin", "*");
+			
+	    }
+		
+		
+       if(response.getHeader("Access-Control-Allow-Headers") == null) {
+			
+			response.addHeader("Access-Control-Allow-Headers", "*");
+			
+	    }
+       
+       
+       if(response.getHeader("Access-Control-Request-Headers") == null) {
+			
+			response.addHeader("Access-Control-Request-Headers", "*");
+			
+	    }
+       
+       
+       
+       if(response.getHeader("Access-Control-Allow-Methods") == null) {
+			
+		   response.addHeader("Access-Control-Allow-Methods", "*");
+			
+	    }
+       
+		
+			
+	}
 	
 	
 	
