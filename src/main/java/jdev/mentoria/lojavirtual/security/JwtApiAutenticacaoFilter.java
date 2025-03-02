6cfbb1554995be2fc2b4f9064e4a07b403592674
User <User@LAPTOP-OJC0P9DU>
2025-03-01 22:28:40 -0300

    alteracao

--- src/main/java/jdev/mentoria/lojavirtual/security/JwtApiAutenticacaoFilter.java
+++ src/main/java/jdev/mentoria/lojavirtual/security/JwtApiAutenticacaoFilter.java
@@ -1,7 +1,48 @@
 package jdev.mentoria.lojavirtual.security;
 
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
-public class JwtApiAutenticacaoFilter {
+/*Intercepta todas as requisicoes do sistema (salvar, deletar, consultar, etc)*/
+public class JwtApiAutenticacaoFilter extends GenericFilterBean {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		
+	 /*Estabelece a autenticacao do user*/
+		
+	  Authentication authentication = new JWTTokenAutenticacaoService().
+			  
+			                          getAuthentication((HttpServletRequest)request, (HttpServletResponse) response);
+	  
+	  
+	  
+	  /*Coloca o processo de autenticacao para o Spring Security*/
+	  
+	  SecurityContextHolder.getContext().setAuthentication(authentication);
+	  
+	  
+	  chain.doFilter(request, response); /*continua o processo/requisicao, chamando a API ou bloqueando*/
+	  
+      	
+		
+	
+	
+	}
+
+   
 
 }
--- src/main/java/jdev/mentoria/lojavirtual/security/WebConfigSecurity.java
+++ src/main/java/jdev/mentoria/lojavirtual/security/WebConfigSecurity.java
@@ -1,36 +1,86 @@
 package jdev.mentoria.lojavirtual.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import jdev.mentoria.lojavirtual.service.ImplementacaoUserDetailsService;
 
 
 @Configuration
 @EnableWebSecurity
 public class WebConfigSecurity {
 	
-	
-	
 
+
+	/*
 	@Bean
     public WebSecurityCustomizer webSecurityCustomizer() throws Exception{
     	
     	
-    	/*URLs para se ignorar (não autenticar)*/
+    	//URLs para se ignorar (não autenticar)
     	 return (web) -> web.ignoring().requestMatchers(HttpMethod.POST, "/salvarAcesso")
     			 
     			 .requestMatchers(HttpMethod.DELETE, "/deleteAcesso");
+	 }
+   */
+   
 	
+	
+	
+	
+    @Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        
+    
+    	http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+        .requestMatchers("/").permitAll()
+        .requestMatchers("/index").permitAll()
+        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+        
+        
+        .anyRequest()
+        .authenticated()
+        
+        		
+        ).formLogin((form) -> form
+                .loginPage("/login")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login-error")
+                .permitAll()
+           ).logout( (logout) -> logout
+                .logoutSuccessUrl("/index")
+           ).exceptionHandling( (ex) -> ex
+                .accessDeniedPage("/negado")
+        
+        
+        
+        );
+        
+        
+        http.addFilterBefore(new JwtApiAutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class)
+        
+        .addFilterAfter(new JWTLoginFilter("/login", this.authenticationManager(null)), UsernamePasswordAuthenticationFilter.class);
+        
+        
+        return http.build();
+
+   
+    
+    
     }
+
 	
 	
 	@Bean
@@ -43,27 +93,17 @@ public class WebConfigSecurity {
 	
 	
 	@Bean
-	public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder, ImplementacaoUserDetailsService userService) throws Exception {
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
 		
-		
-	  return http.getSharedObject(AuthenticationManagerBuilder.class)
-			  
-	   .userDetailsService(userService)
-	   
-	   .passwordEncoder(passwordEncoder)
-	   
-	   .and()
-	   
-	   .build();
+		return authenticationConfiguration.getAuthenticationManager();
+
 		
 		
 		
 	}
 	
 	
-   
-
-	
+ 
 
 }
 	
