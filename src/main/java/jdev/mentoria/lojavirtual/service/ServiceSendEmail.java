cd424458977b442ad0c399b655cc5826c7044d99
User <User@LAPTOP-OJC0P9DU>
2025-03-16 04:09:11 -0300

    primeiro commit 16-03-2025

---
+++ src/main/java/jdev/mentoria/lojavirtual/service/ServiceSendEmail.java
@@ -0,0 +1,28 @@
+package jdev.mentoria.lojavirtual.service;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceSendEmail {
+
+  
+    private String userName = "l05409337@gmail.com";
+    
+    private String senha = "EngAdm123***";
+    
+    
+   
+    @Async
+    public void enviarEmailHtml(String assunto, String mensagem, String emailDestino) {
+    	
+    	
+    	
+    	
+    	
+    	
+    }
+
+
+
+}
