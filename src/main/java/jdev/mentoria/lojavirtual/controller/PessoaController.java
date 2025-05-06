fec3034676f0a78852510c35140812ea0d9a26b6
User <User@LAPTOP-OJC0P9DU>
2025-05-06 16:27:22 -0300

    commit 06-05-2025

--- src/main/java/jdev/mentoria/lojavirtual/controller/PessoaController.java
+++ src/main/java/jdev/mentoria/lojavirtual/controller/PessoaController.java
@@ -52,7 +52,7 @@ public class PessoaController {
 		
 		List<PessoaFisica> fisicas = pesssoaFisicaRepository.pesquisaPorNomePF(nome.trim().toUpperCase());
 		
-		//jdbcTemplate.execute("begin; update tabela_acesso_end_potin set qtd_acesso_end_point = qtd_acesso_end_point + 1 where nome_end_point = 'END-POINT-NOME-PESSOA-FISICA'; commit;");
+		jdbcTemplate.execute("begin; update tabela_acesso_end_potin set qtd_acesso_end_point = qtd_acesso_end_point + 1 where nome_end_point = 'END-POINT-NOME-PESSOA-FISICA'; commit;");
 		
 		return new ResponseEntity<List<PessoaFisica>>(fisicas, HttpStatus.OK);
 	}
