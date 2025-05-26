7847022f55d24e9e5c1925b46c757de49f238f76
User <User@LAPTOP-OJC0P9DU>
2025-05-26 10:00:50 -0300

    commit 26-05-2025

--- src/main/java/jdev/mentoria/lojavirtual/model/Produto.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/Produto.java
@@ -107,6 +107,24 @@ public class Produto implements Serializable {
 	@JoinColumn(name = "marca_produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "marca_produto_id_fk"))
 	private MarcaProduto marcaProduto = new MarcaProduto();
 	
+	
+	
+	@NotNull(message = "Nota Item do produto deve ser informado.")
+	@ManyToOne(targetEntity = NotaItemProduto.class)
+	@JoinColumn(name = "nota_item_produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "nota_item_produto_id_fk"))
+	private NotaItemProduto notaItemProduto = new NotaItemProduto();
+	
+	
+	
+	
+	public void setNotaItemProduto(NotaItemProduto notaItemProduto) {
+		this.notaItemProduto = notaItemProduto;
+	}
+	
+	
+	public NotaItemProduto getNotaItemProduto() {
+		return notaItemProduto;
+	}
 	     
 	
 	public void setMarcaProduto(MarcaProduto marcaProduto) {
